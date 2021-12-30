850d81fc8cbcecc7fbbe387bc2a02b3040b755f9
theLeroy <janis.wanger@novu.ch>
2021-12-30 23:51:20 +0100

    Update FormatCard.vue

--- components/FormatCard.vue
+++ components/FormatCard.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="w-full bg-[#212121] h-auto my-2 p-2 rounded mx-1 text-white">
     <div class="grid row-2">
-      <div>prefix: {{ font.prefix }}</div>
-      <div>
-        classes:
+      <div class="flex justify-between">
+        <label id="prefix">Prefix: {{font.prefix}}</label>
+        <input
+          id="prefix"
+          v-model="font.prefix"
+          type="text"
+          class="bg-none bg-opacity-0 rounded px-2 py-1"
+        />
+      </div>
+      <div class="mt-2">
+        
+        <div>Classes:</div>
         <span v-for="(item, index) in font.classes" :key="index">
-          {{ item }}
+         {{font.prefix + item }}
         </span>
       </div>
     </div>
@@ -22,3 +31,10 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+input {
+  background-color: rgba(0, 0, 0, 0);
+  outline: rgba(255, 255, 255, 0.5 ) solid 1px;
+}
+</style>
