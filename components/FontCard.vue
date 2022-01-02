74ec785fb73f2bd0da99c4b2e0113afb106edc6c
theLeroy <janis.wanger@novu.ch>
2022-01-02 19:08:56 +0100

    update interface

--- components/FontCard.vue
+++ components/FontCard.vue
@@ -4,7 +4,7 @@
     <draggable
       group="fonts"
       :list="group"
-      class="bg-[#383838] shadow-md h-80 p-2"
+      class="bg-[#383838] shadow-md h-[450px] p-2"
     >
       <div class="list-group-item" v-for="(font, index) in group" :key="index">
         <FormatCard :font="font" />
